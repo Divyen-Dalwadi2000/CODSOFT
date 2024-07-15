c037eb04e4113521405ac80d0704f134d200e9f6
Mrdivyendalwadi <114602971+Divyen-Dalwadi2000@users.noreply.github.com>
2024-07-16 00:58:23 +0530

    make resume download component

--- react-portfolio/src/components/About.jsx
+++ react-portfolio/src/components/About.jsx
@@ -2,15 +2,17 @@ import Aboutprofile from "../assets/1.png"
 import { ABOUT_TEXT } from "../constants/index";
 import { motion } from "framer-motion"
 import { ABOUT_IMG } from "../constants/index";
+import Resumebutton from "./Resumebutton";
+
 const About = () => {
     return (
         <>
             <div className="border-b border-neutral-900 pb-4">
                 <motion.h1
-                 whileInView={{y:0 , opacity:1}}
-                 initial={{y:-100 , opacity:0}}
-                 transition={{duration:1.5}}
-                className="my-20 text-center text-4xl">About<span className="text-neutral-500 bg-gradient-to-r from-pink-500  to-purple-500 bg-clip-text text-transparent"> Me</span>
+                    whileInView={{ y: 0, opacity: 1 }}
+                    initial={{ y: -100, opacity: 0 }}
+                    transition={{ duration: 1.5 }}
+                    className="my-20 text-center text-4xl">About<span className="text-neutral-500 bg-gradient-to-r from-pink-500  to-purple-500 bg-clip-text text-transparent"> Me</span>
                 </motion.h1>
                 <div className="flex flex-wrap ">
                     <div className="w-full lg:w-1/2 lg:p-8">
@@ -28,8 +30,9 @@ const About = () => {
                                 whileInView={{ x: 0, opacity: 1 }}
                                 initial={{ x: 100, opacity: 0 }}
                                 transition={{ duration: 1, delay: 0.5 }}
-                                className="my-2 max-w-xl py-20 font-light">{ABOUT_TEXT} </motion.p>
+                                className="my-1 max-w-xl py-20 font-light">{ABOUT_TEXT} </motion.p>
                         </div>
+                            <Resumebutton />
                     </div>
                 </div>
             </div>
